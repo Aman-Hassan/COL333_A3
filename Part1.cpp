5d29b59d79a379f8e060365f2ceeced8f2116aec
Kartik Meena <109080120+alkartik@users.noreply.github.com>
2023-10-05 19:12:33 +0530

    new constraints added

--- Part1.cpp
+++ Part1.cpp
@@ -34,24 +34,24 @@ int main(){
                 clauses.push_back(-(j));    
                 cnf.push_back(clauses);  
                 clauses.clear();
-                clauses.push_back(i+N);
-                clauses.push_back(j+N);
+                clauses.push_back(-(i+N));
+                clauses.push_back(-(j+N));
                  cnf.push_back(clauses); 
             }
         }
     }
     //constraint 3 : ensuring the size of subgraphs as K1 and K2 
-    // for G1 graph
-    vector<vector<bool>> S;   //making a counter Sij hat represent that at least j of the variables x1,x2,x3...xi are assigned to true.
-    for(int i=0;i<=N;i++){
-        S[i][0] = 1;         // when subgraph G1 of size 0 is selected form graph having 0...N vertices
+    // for G1 graph   at least condition
+    vector<vector<int>> S;   //making a counter Sij hat represent that at least j of the variables x1,x2,x3...xi are assigned to true.
+    for(int i=1;i<=N;i++){
+        S[i][1] = 1;         // when subgraph G1 of size 0 is selected form graph having 1...N vertices
     }
-    for(int j=1;j<=K1;j++){
-       S[0][j] = 0;        //when G has size 0 , then no subgraph is possible
+    for(int j=2;j<=K1;j++){
+        S[1][j] = 0;       //when G has size 1 , then no subgraph is possible
     }
     // Converted the relation sij iff (s(i-1)(j) | (i & s(i-1)(j-1))) for 1<=i<=N and 1<=j<=K1  into CNF form 
-    for(int i=1;i<=N;i++){
-        for(int j=1;j<=K1;j++){
+    for(int i=2;i<=N;i++){
+        for(int j=2;j<=K1;j++){
             vector<int> clauses;
             clauses.push_back(-(S[i][j]));
             clauses.push_back((S[i-1][j]));
@@ -74,13 +74,13 @@ int main(){
 
         }
     }
-    // for G2 graph 
+    // for G2 graph at least K2 condition 
    for(int i=1;i<=N;i++){
-        S[i+N][0] = 1;         // when subgraph of size 0 is selected form graph having 0...N vertices
+        S[i+N][1] = 1;         // when subgraph of size 0 is selected form graph having 0...N vertices
     }
-    //as K1 >= K2 hence second condition is already done for K2
+    //as K1 >= K2 hence second condition is already done for K2                                                          
      for(int i=2;i<=N;i++){
-        for(int j=1;j<=K2;j++){
+        for(int j=2;j<=K2;j++){
             vector<int> clauses;
             clauses.push_back(-(S[i+N][j]));
             clauses.push_back((S[i-1+N][j]));
@@ -102,6 +102,56 @@ int main(){
              cnf.push_back(clauses);
         }
     }
+    S[N][K1] = 1;          // To enforce that the found clique contains at least K1  vertices
+    S[2*N][K1]  = 1;       
+
+
+    // at most condition of K1 
+    vector<vector<int> M; 
+    vector<int> r ;
+    r.push_back(1);
+    r.push_back(M[1][1]);
+    cnf.push_back(r);   
+    r.clear();
+    for(j=2;j<=K1;j++){
+        r.push_back(-(M[1][j]));
+        cnf.push_back(r);
+        r.clear();
+    } 
+    for(int i=2;i<N;i++){
+        r.push_back(-(i));
+        r.push_back(M[i][1]);
+        cnf.push_back(r);
+         r.clear();
+         r.push_back(-(M[i-1][1]));
+         r.push_back((M[i][1]));
+          cnf.push_back(r);
+         r.clear();
+         r.push_back(-(i));
+         r.push_back(-(M[i-1][K1]));
+         cnf.push_back(r);
+         r.clear();
+    }
+    for(int i=2;i<N;i++){
+        for(int j=2;j<=K1;j++){
+            r.push_back(-i);
+            r.push_back(-M[i-1][j-1]);
+            r.push_back(M[i][j]);
+            cnf.push_back(r);
+            r.clear();
+            r.push_back(-M[i-1][j]);
+            r.push_back(M[i][j]);
+            cnf.push_back(r);
+            r.clear();
+        }
+    }
+    r.push_back(-(N));
+    r.push_back(-(M[N-1][K1]));
+    cnf.push_back(r);
+    r.clear();
+    // same for K2 also 
+
+
 
     cout << "p cnf " << 2*N + N*K1 + N*K2 << " " << cnf.size() << endl;
     for (const vector<int>& it : cnf) {
