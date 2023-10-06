fad2d6ac39638883b7cee77e0a5b578fee5cb091
Kartik Meena <109080120+alkartik@users.noreply.github.com>
2023-10-07 00:02:37 +0530

    all conditions added

--- Part1.cpp
+++ Part1.cpp
@@ -16,7 +16,7 @@ int main(){
         edges[egde] = 1;  // initially assuming all edges are mappend in G1
     }
     int total_varibles = 2*N*K1 + 2*N*K2;
-    int total_clauses;
+    int total_clauses  = N + N*(N-1)/2 - E + 3*(N-1) + K1 -1 + 6*(N-1)*(K1-1) + 3*(N-1) + K2 - 1 + 6*(N-1)*(K2-1)
     cout << "c" << " " << "comment" << endl;
     cout << "p cnf" << " " << total_varibles << " " << total_clauses << endl;
     // Constraint 1 : each vertex either in G1 or G2 but not in both
@@ -35,23 +35,23 @@ int main(){
     }
     //constraint 3 : ensuring the size of subgraphs as K1 and K2 
     // for G1 graph   at most condition K1
+    // implemented si,j = i+j*N+offset1
     int offset1 = N*K1;
-    cout << -1 << " " << 1 + 1*N + offset1 << " " << 0 << endl;
     for(int j=2;j<=K1;j++){
         cout << - (1 + j*N + offset1 ) << " " << 0 << endl; 
     }
-    for(int i=2;i<N;i++){
+    for(int i=1;i<=N;i++){
        cout << -i << " " << i + 1*N + offset1 << " " << 0 << endl;
     }
-    for(int i=2;i<N;i++){
+    for(int i=2;i<=N;i++){
           cout << -(i-1 + 1*N + offset1) << " " << i + 1*N + offset1 << " " << 0 << endl;
     }
-    for(int i=2;i<N;i++){
+    for(int i=2;i<=N;i++){
         for(int j=2;j<=K1;j++){
             cout << -(i+(j)*N+offset1) << " " << i+j*N+offset1 << " " << 0 << endl;
         }
     }
-    for(int i=2;i<N;i++){
+    for(int i=2;i<=N;i++){
         for(int j=2;j<=K1;j++){
            cout << -i << " " << -(i-1 + (j-1)*N + offset1) << " " << i + j*N + offset1 << " " << 0 << endl;
         }
@@ -60,14 +60,48 @@ int main(){
         cout << -i << " " << -(i-1 + K1*N + offset1) << " " << 0 << endl;
     }
    // at least K1 is true  
-   for(int i=1;i<=N;i++){
-    for(int j=1;j<=K1;j++){
+   for(int i=2;i<=N;i++){
+    for(int j=2;j<=K1;j++){
       cout << -(i+j*N+offset1) << " "  << (i-1 + j*N + offset1) << " " << i << " " << 0 << endl;
       cout << -(i+j*N+offset1) << " "  << (i-1 + j*N + offset1) << " " << (i-1 +(j-1)*N + offset1) << " " << 0 << endl;
       cout << -(i-1+j*N+offset1) << " "  << (i + j*N + offset1) << " " << 0 << endl;
       cout << -i << " " << -(i-1 + (j-1)*N + offset1) << " " << (i+j*N+offset1) << " " << 0 << endl;
     }   
    }
-  
+   int offset2 = N*K2;  
+    // for G2 graph   at most condition K2  
+    // implemented si,j = i+ N +j*N+offset2
+     for(int j=2;j<=K2;j++){
+          cout << - (1+N + j*N + offset2 ) << " " << 0 << endl; 
+     }
+     for(int i=1;i<N;i++){
+         cout << -(i+N) << " " << (i+N) + 1*N + offset2 << " " << 0 << endl;
+     }
+     for(int i=2;i<N;i++){
+             cout << -(i-1+N + 1*N + offset2) << " " << i +N+ 1*N + offset2 << " " << 0 << endl;
+     }
+     for(int i=2;i<N;i++){
+          for(int j=2;j<=K2;j++){
+                cout << -(i+ N +(j)*N+offset2) << " " << (i+N) +j*N+offset2 << " " << 0 << endl;
+          }
+     }
+     for(int i=2;i<N;i++){
+          for(int j=2;j<=K2;j++){
+              cout << -(i+N) << " " << -(i-1 + N + (j-1)*N + offset2) << " " << (i + N + j*N + offset2) << " " << 0 << endl;
+          }
+     }
+     for(int i=2;i<=N;i++){
+          cout << -(i +N) << " " << -(i-1 + N + K2*N + offset2) << " " << 0 << endl;
+     }
+        // at least K2 is true  
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=K2;j++){
+                cout << -(i+N +j*N+offset2) << " "  << (i-1 + N + j*N + offset2) << " " << i +N << " " << 0 << endl;
+                cout << -(i + N +j*N+offset2) << " "  << (i-1 + N + j*N + offset2) << " " << (i-1 + N +(j-1)*N + offset2) << " " << 0 << endl;
+                cout << -(i-1 + N +j*N+offset2) << " "  << (i + N + j*N + offset2) << " " << 0 << endl;
+                cout << -(i +N) << " " << -(i-1 + N + (j-1)*N + offset2) << " " << (i + N + j*N+offset2) << " " << 0 << endl;
+            }   
+        }
+
 
 }
